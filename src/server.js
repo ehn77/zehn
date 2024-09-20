420e995e26677328cb0a872c4b8fa963eac622eb
Ethan <nguyeneh77@gmail.com>
2024-09-20 16:00:37 -0400

    v2

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 - Page Not Found');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
