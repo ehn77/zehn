51d741bc360e208fd6cad0fe8adbf57c85259436
Ethan <nguyeneh77@gmail.com>
2024-09-20 16:10:35 -0400

    v3

--- src/server.js
+++ src/server.js
@@ -4,10 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.use((req, res) => {
